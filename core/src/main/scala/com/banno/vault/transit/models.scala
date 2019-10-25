498d05d1d10ff9c446a96d94de4244453a099bad
Diego Alonso <diego.blas@banno.com>
2019-10-25 00:00:00 +0000

    Add Vault-Transit client support.
    Vault Transit is a secrets engine that provides "encryption as a
    service": it allows creating (on the server) encryption keys with a
    specific algorithm, and offers an HTTP API with endpoints to encrypt
    or decrypt data, relieving client from the need to handle keys (which
    is risky) or implement cryptosystems (which is error-prone).
    This commit adds support for transit endpoints. In particulat:
    
    - We add a transit-client class, made of a HTTP Client, a key name,
      and the vault base URI. The ideal use of this Transit-Client is as
      a representation of a transit connection, which hides all of the
      transit logic and only provides encryption/decryption functions.
    
    - In line with the design of the Vault client, we also add a Transit
      object that provides the main functions separately.
    
    - We add a set of models, to represent the requests and responses of
      the Vault-transit endpoints, and their Json encoding and decoding.
    
    - We add a small wrapper type for Base64-Encoded strings, since that
      is what Vault-Transit uses for contexts and plaintext values.
      In this library we will not deal with encoding/decoding of other
      values into Base64: we refer to `scodec` library for that.
    
    - We add some wrapper-clases for contexts and plaintexts, to tell them
      apart in the type level. We also add a wrapper class for the
      Ciphertext, but on this one we make no assumptions about the inner string.
    
    In this library we will not deal with encoding/decoding of other
    data types into Base64. Doing that was considered at first, but there
    were some reasons not to do so:
    - This library is a Scala representation of Vault api, so there is no
      need to do strictly more than a Vault client would do.
    - Other libraries like scodec already define such type-class with
      a rich set of instances, so no point in inventing a training wheel.

---
+++ core/src/main/scala/com/banno/vault/transit/models.scala
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2019 Jack Henry & Associates, Inc.®
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.banno.vault.transit
+
+import cats.Eq
+import cats.kernel.instances.option._
+import cats.kernel.instances.string._
+import cats.syntax.eq._
+import io.circe.{Decoder, Encoder, Json}
+import java.time.Instant
+
+final case class KeyName(name: String)
+final case class KeyDetails(
+  name: String,
+  isConvergent: Boolean,
+  isDerived: Boolean,
+  versions: Map[Int, Instant],
+  cipher: String
+)
+
+object KeyDetails {
+
+  implicit final val encodeKeyDetails: Encoder[KeyDetails] =
+    Encoder.instance { (kd: KeyDetails) => Json.obj(
+      "data" -> Json.obj(
+        "name"                  -> Json.fromString(kd.name),
+        "type"                  -> Json.fromString(kd.cipher),
+        "convergent_encryption" -> Json.fromBoolean(kd.isConvergent),
+        "derived"               -> Json.fromBoolean(kd.isDerived),
+        "keys"                  -> Json.fromFields(
+          kd.versions.map { case (num, inst) =>
+            num.toString -> Json.fromLong(inst.getEpochSecond)
+          }
+        )
+      )
+    )}
+
+    implicit final val decodeKeyDetails: Decoder[KeyDetails] = {
+      implicit val decodeInstantSecond: Decoder[Instant] =
+        Decoder.decodeLong.emap { numsec =>
+          if (numsec <= Instant.MAX.getEpochSecond)
+            Right(Instant.ofEpochSecond(numsec))
+          else Left(s"value $numsec of UNIX epoch seconds is over Java maximum Instant")
+        }
+      Decoder.instance[KeyDetails] { c =>
+        Decoder.resultInstance.map5(
+          c.downField("data").downField("name").as[String],
+          c.downField("data").downField("convergent_encryption").as[Boolean],
+          c.downField("data").downField("derived").as[Boolean],
+          c.downField("data").downField("keys").as[Map[Int, Instant]],
+          c.downField("data").downField("type").as[String]
+        )(KeyDetails.apply(_,_,_,_,_))
+      }
+    }
+
+}
+
+/** A tagged-like newtype used to indicate that a Base64 value is a plaintext we want to encrypt.
+  */
+final case class PlainText(plaintext: Base64)
+object PlainText {
+  implicit val eqPlainText: Eq[PlainText] =
+    Eq.by[PlainText, Base64](_.plaintext)
+  private[transit] implicit val encodePlainText: Encoder[PlainText] =
+    Base64.encodeBase64.contramap(_.plaintext)
+  private[transit] implicit val decodePlainText: Decoder[PlainText] =
+    Base64.decodeBase64.map(PlainText.apply)
+}
+
+/** A tagged-like newtype used to indicate that a Base64 value is the user-supplied context used in key derivation.
+  * "Key derivation allows the same key to be used for multiple purposes
+  * by deriving a new key based on a user-supplied context value
+  *
+  * https://www.vaultproject.io/docs/secrets/transit/index.html#transit-secrets-engine
+  */
+final case class Context(context: Base64)
+object Context {
+  implicit val eqContext: Eq[Context] =
+    Eq.by[Context, Base64](_.context)
+  private[transit] implicit val encodeContext: Encoder[Context] =
+    Base64.encodeBase64.contramap(_.context)
+  private[transit] implicit val decodeContext: Decoder[Context] =
+    Base64.decodeBase64.map(Context.apply)
+}
+
+/** In the Vault Transit, cipher-texts are Base64 strings preceded by the `"vault:v1:"` prefix text.
+  * We our special wrapper class to represent Base64 Strings.
+  */
+final case class CipherText(ciphertext: String) extends AnyVal
+object CipherText {
+  private[transit] implicit val encodeCipherText: Encoder[CipherText] =
+    Encoder.encodeString.contramap(_.ciphertext)
+  private[transit] implicit val decodeCipherText: Decoder[CipherText] =
+    Decoder.decodeString.map(CipherText.apply)
+  implicit val eqCipherText: Eq[CipherText] =
+    Eq.by[CipherText, String](_.ciphertext)
+}
+
+final case class EncryptRequest(plaintext: PlainText, context: Option[Context])
+object EncryptRequest {
+  implicit val eqEncryptRequest: Eq[EncryptRequest] =
+    Eq.instance { (x: EncryptRequest, y: EncryptRequest) =>
+      x.context === y.context && x.plaintext === y.plaintext
+    }
+  implicit val encodeEncryptRequest: Encoder[EncryptRequest] =
+    Encoder.forProduct2("plaintext", "context")(er => (er.plaintext, er.context))
+  implicit val decodeEncryptRequest: Decoder[EncryptRequest] =
+    Decoder.forProduct2("plaintext", "context")((pt: PlainText, ct: Option[Context]) => EncryptRequest(pt,ct))
+}
+
+private[transit] final case class EncryptResult(ciphertext: CipherText)
+private[transit] object EncryptResult {
+  implicit val eqEncryptResult: Eq[EncryptResult] = Eq.by(_.ciphertext)
+
+  implicit val encodeEncryptResult: Encoder[EncryptResult] =
+    Encoder.instance { (er: EncryptResult) => Json.obj(
+      "ciphertext" -> Encoder[CipherText].apply(er.ciphertext)
+    )}
+
+  implicit val decodeEncryptResult: Decoder[EncryptResult] =
+    Decoder.forProduct1("ciphertext")((ct: CipherText) => EncryptResult.apply(ct))
+}
+
+private[transit] case class EncryptResponse(data: EncryptResult)
+private[transit] object EncryptResponse {
+  implicit val eqEncryptResponse: Eq[EncryptResponse] =
+    Eq.by[EncryptResponse, EncryptResult](_.data)
+  implicit val encodeEncryptResponse: Encoder[EncryptResponse] =
+    Encoder.forProduct1("data")(_.data)
+  implicit val decodeEncryptResponse: Decoder[EncryptResponse] =
+    Decoder.forProduct1("data")((d: EncryptResult) => EncryptResponse(d))
+}
+
+private[transit] final case class DecryptRequest(ciphertext: CipherText, context: Option[Context])
+private[transit] object DecryptRequest {
+  implicit val eqDecryptRequest: Eq[DecryptRequest] =
+    Eq.instance { (x: DecryptRequest, y: DecryptRequest) =>
+      x.context === y.context && x.ciphertext === y.ciphertext
+    }
+
+  implicit val encodeDecryptRequest: Encoder[DecryptRequest] =
+    Encoder.forProduct2("ciphertext", "context")(dr => (dr.ciphertext, dr.context))
+
+  implicit val decodeDecryptRequest: Decoder[DecryptRequest] =
+    Decoder.forProduct2("ciphertext", "context")((cr: CipherText, ct: Option[Context]) => DecryptRequest(cr, ct))
+}
+
+private[transit] final case class DecryptResult(plaintext: PlainText)
+private[transit] object DecryptResult {
+  implicit val eqDecryptResponse: Eq[DecryptResult] =
+    Eq.by[DecryptResult, PlainText](_.plaintext)
+
+  implicit val encodeDecryptResult: Encoder[DecryptResult] =
+    Encoder.forProduct1("plaintext")(_.plaintext)
+
+  implicit val decodeDecryptResult: Decoder[DecryptResult] =
+    Decoder.forProduct1("plaintext")((pt: PlainText)  => DecryptResult(pt))
+}
+
+private[transit] final case class DecryptResponse(data: DecryptResult)
+private[transit] object DecryptResponse {
+  implicit val eqDecryptResponse: Eq[DecryptResponse] =
+    Eq.by[DecryptResponse, DecryptResult](_.data)
+
+  implicit val encodeDecryptResponse: Encoder[DecryptResponse] =
+    Encoder.forProduct1("data")(_.data)
+  implicit val decodeDecryptResponse: Decoder[DecryptResponse] =
+    Decoder.forProduct1("data")((d: DecryptResult) => DecryptResponse(d))
+}
+
+private[transit] final case class TransitError(error: String)
+private[transit] object TransitError {
+  type Or[A] = Either[TransitError, A]
+
+  implicit val encodeError: Encoder[TransitError] =
+    Encoder.forProduct1("error")(_.error)
+
+  implicit val decodeError: Decoder[TransitError] =
+    Decoder.forProduct1("error")((e: String) => TransitError(e))
+
+  implicit def encodeOr[A](implicit encodeA: Encoder[A]): Encoder[Or[A]] =
+    Encoder.instance {
+      case Left(err) => encodeError(err)
+      case Right(a)  => encodeA(a)
+    }
+
+  implicit def decodeOr[A](implicit decodeA: Decoder[A]): Decoder[Or[A]] =
+    decodeError either decodeA
+
+}
