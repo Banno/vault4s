259c9615555a69ec628e8cc26452663f3f91d16d
Zach McCoy <zach.mccoy@banno.com>
2021-06-29 17:23:11 -0500

    Adding in files that had merge conflicts / needed changes

--- core/src/test/scala/com/banno/vault/VaultSpec.scala
+++ core/src/test/scala/com/banno/vault/VaultSpec.scala
@@ -21,11 +21,13 @@ import java.util.concurrent.TimeUnit
 
 import cats.effect.{IO, Sync}
 import cats.implicits._
-import com.banno.vault.models.{CertificateData, CertificateRequest, VaultSecret, VaultSecretRenewal, VaultToken}
+import com.banno.vault.models.{CertificateData, CertificateRequest, VaultSecret, VaultSecretRenewal, VaultToken, VaultKeys}
 import io.circe.Decoder
 import org.http4s._
 import org.http4s.implicits._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import org.http4s.util.CaseInsensitiveString
 import org.http4s.circe._
 import org.http4s.client.Client
 
@@ -82,6 +84,8 @@ class VaultSpec extends ScalaCheckSuite {
 
   implicit val certificateRequestDecoder: Decoder[CertificateRequest] =
     Decoder.forProduct7("common_name", "alt_names", "ip_sans", "ttl", "format", "private_key_format", "exclude_cn_from_sans")(CertificateRequest.apply)
+
+  object ListQueryParamMatcher extends QueryParamDecoderMatcher[String]("list")
 
   val certificate: String      = UUID.randomUUID().toString
   val issuing_ca: String       = UUID.randomUUID().toString
@@ -270,6 +274,15 @@ class VaultSpec extends ScalaCheckSuite {
                   |}""".stripMargin)
           }
         }
+      case req @ GET -> Root / "v1" / "secret" / "postgres" / "" :? ListQueryParamMatcher(_) =>
+        checkVaultToken(req){
+          Ok(s"""
+                |{
+                | "data": {
+                |   "keys": ["postgres1", "postgres-pupper"]
+                | }
+                |}""".stripMargin)
+        }
 
       case GET -> path =>
         BadRequest(s"Path not mapped: $path")
@@ -409,11 +422,17 @@ class VaultSpec extends ScalaCheckSuite {
     }
   }
 
-  property("renewToken works as expected when sending a valid token") {
-    Prop.forAll(VaultArbitraries.validVaultUri){uri =>
-      Vault.renewSelfToken[IO](mockClient, uri)(VaultToken(clientToken, 3600, true), 1.hour)
-        .unsafeRunSync() === VaultToken(clientToken, 3600, renewable)
-    }
+property("listSecrets works as expected when requesting keys under path") {
+  Prop.forAll(VaultArbitraries.validVaultUri){uri =>
+    Vault.listSecrets[IO](mockClient, uri)(clientToken, "/secret/postgres/")
+      .unsafeRunSync() == VaultKeys(List("postgres1", "postgres-pupper"))
+  }
+}
+
+property("renewToken works as expected when sending a valid token") {
+  Prop.forAll(VaultArbitraries.validVaultUri){uri =>
+    Vault.renewSelfToken[IO](mockClient, uri)(VaultToken(clientToken, 3600, true), 1.hour)
+      .unsafeRunSync() === VaultToken(clientToken, 3600, renewable)
   }
 
   property("revokeToken works as expected when revoking a valid token") {
